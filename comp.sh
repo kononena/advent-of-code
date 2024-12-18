6f0b4a5a113a02c61a745755e22ace0a176af3cb
Aleksi Kononen <aleksikono@gmail.com>
2024-12-18 21:02:08 +0200

    Fix compilation commands
    Include the utils directory

--- comp.sh
+++ comp.sh
@@ -2,7 +2,7 @@
 
 if [[ $# < 2 ]] ; then
   echo -e "compiling \033[1;34mutils\033[0m"
-  g++ -O3 -c ./utils/utils.cpp -o ./build/utils.o
+  g++ -O3 -I./utils -c ./utils/utils.cpp -o ./build/utils.o
   exit
 fi
 
@@ -12,4 +12,4 @@ if ! [ -d "$1/$2" ]; then
 fi
 
 echo -e "compiling \033[1;34m$1/$2/solution.cpp\033[0m"
-g++ -O3 ./build/utils.o ./$1/$2/solution.cpp -o ./build/$1_$2
+g++ -O3 -I./utils ./build/utils.o ./$1/$2/solution.cpp -o ./build/$1_$2
