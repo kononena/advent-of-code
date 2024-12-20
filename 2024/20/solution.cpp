b37620240e17e66c1819c5069603fcca005086ea
aleksi@work-laptop <aleksikono@gmail.com>
2024-12-20 12:52:58 +0200

    Solve 2024/20 P1

---
+++ 2024/20/solution.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "utils.hpp"
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    std::cerr << "Provide filename" << std::endl;
+    return 1;
+  }
+  std::string input(argv[1]);
+  std::string filename;
+  if (input[0] == 'e')
+    filename = "example.txt";
+  else if (input[0] == 'i')
+    filename = "input.txt";
+  
+  std::cout << "Using file " << filename << std::endl;
+  std::ifstream file;
+  file.open(filename, std::ios::in);
+  if (!file.is_open()) {
+    std::cerr << "Error opening file" << std::endl;
+    return 1;
+  }
+
+  bool is_input = filename == "input.txt";
+  utils::Timer timer;
+
+  int nxy = 15;
+  if (is_input)
+    nxy = 141;
+  
+  std::vector<char> map;
+  int sx, sy, ex, ey;
+  utils::read_grid_2D(file, map, nxy, nxy, 0, '#');
+  for (int y = 0; y < nxy; y++)
+  for (int x = 0; x < nxy; x++) {
+    if (map[x + y * nxy] == 'S') {
+      sx = x; sy = y;
+      map[x + y * nxy] = '.';
+    } else if (map[x + y * nxy] == 'E') {
+      ex = x; ey = y;
+      map[x + y * nxy] = '.';
+    }
+  }
+  
+  /* Part 1 */
+  std::vector<int> scores(nxy * nxy, nxy * nxy);
+  scores[sx + sy * nxy] = 0;
+  int x = sx, y = sy;
+  int next_x, next_y;
+  int score;
+  const int dir[4] = { 1, -1, nxy, -nxy };
+  while (!(x == ex && y == ey)) {
+    score = scores[x + y * nxy];
+    for (int i = 0; i < 4; i++) {
+      int target = x + y * nxy + dir[i];
+      if (map[target] == '.' && scores[target] > score) {
+        next_x = (target) % nxy;
+        next_y = (target) / nxy;
+        scores[target] = std::min(scores[target], score + 1);
+      }
+    }
+
+    x = next_x; y = next_y;
+  }
+
+  long good_cheats = 0L;
+  int minimum_save = 2;
+  if (is_input)
+    minimum_save = 100;
+  for (int y = 1; y < nxy - 1; y++)
+  for (int x = 1; x < nxy - 1; x++)
+  if (map[x + y * nxy] == '#') {
+    for (int i = 0; i < 4; i++)
+    for (int j = i+1; j < 4; j++) {
+      int target_i = x + y * nxy + dir[i];
+      int target_j = x + y * nxy + dir[j];
+      if (scores[target_i] < nxy * nxy && scores[target_j]< nxy * nxy && std::abs(scores[target_i] - scores[target_j]) - 2 >= minimum_save)
+        good_cheats++;
+    }
+  }
+
+  std::cout << "Part 1\n  Cheats saving over " << minimum_save << " picoseconds : " << good_cheats << std::endl;
+
+  /* Part 2 */
+
+  std::cout << "Part 2\n  Solution : " << std::endl;
+
+  return 0;
+}
